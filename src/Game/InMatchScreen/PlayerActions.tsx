be7976d3c7b3923d90540c5b7a6a75c05caf4a55
Chris Millward <cmillward@hey.com>
2024-01-10 08:25:26 -0500

    Sample action

--- src/Game/InMatchScreen/PlayerActions.tsx
+++ src/Game/InMatchScreen/PlayerActions.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useShallow } from 'zustand/react/shallow';
 
 import { Button, Stack } from '../../UI';
+import { backUpAction } from '../actions';
+import { useInMatchContext } from './InMatchContext';
 
 export const PlayerActions: React.FC = () => {
+  const { wrestler1, wrestler2, updateWrestler1, updateWrestler2 } =
+    useInMatchContext(
+      useShallow(
+        ({ wrestler1, wrestler2, updateWrestler1, updateWrestler2 }) => ({
+          wrestler1,
+          wrestler2,
+          updateWrestler1,
+          updateWrestler2,
+        })
+      )
+    );
+
   return (
     <Stack gap={8}>
-      <Button>Back up</Button>
+      <Button
+        onClick={() => {
+          const result = backUpAction.onPerform(wrestler1, wrestler2);
+          updateWrestler1(result.performer);
+          if (result.receiver) {
+            updateWrestler2(result.receiver);
+          }
+        }}
+      >
+        Back up
+      </Button>
       <Button>Kick</Button>
       <Button>Lock up</Button>
       <Button>Run against the ropes</Button>
